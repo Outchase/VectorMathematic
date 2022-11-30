4e62382b8c2f3f811e32bb159b090c4437ba79ce
Outchase <101480664+Outchase@users.noreply.github.com>
2022-11-30 22:41:20 +0100

    functions added and vektor class created

--- VectorMathematicSol/VectorMathematic/Program.cs
+++ VectorMathematicSol/VectorMathematic/Program.cs
@@ -6,7 +6,18 @@ namespace VectorMathematic
     {
         static void Main(string[] args)
         {
+            Vektors vectorA = new Vektors();
+            Vektors vectorB = new Vektors();
+
             Console.WriteLine("Hello World!");
+        }
+
+        static float CalculateVectorDistance(Vektors vectorA, Vektors vectorB)
+        {
+            float result = 0;
+
+
+            return result;
         }
     }
 }
---
+++ VectorMathematicSol/VectorMathematic/Vektors.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorMathematic
+{
+    internal class Vektors
+    {
+        public float x, y, z;
+
+        float vectorLength = CalculateVectorLength(x, y, z);
+
+        public Vektors()
+        {
+            this.x = 0;
+            this.y = 0;
+            this.z = 0;
+        }
+
+        public Vektors(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+
+        }
+
+
+        public float CalculateVectorLength(float x, float y, float z)
+        {
+            float result = 0;
+
+            //float baseSquare = CalculateSquareLength();
+            float heightSquare = CalculateSquareLength(z);
+
+            
+
+            return result;
+        }
+
+        public float CalculateSquareLength(float addend)
+        {
+            float result = (float)Math.Pow(addend,2);
+
+            return result;
+        }
+    }
+}
